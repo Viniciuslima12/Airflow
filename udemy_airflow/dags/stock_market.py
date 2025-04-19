b04c76e8de2a89258e3ffee734558ad70ac96f7e
Viniciuslima12 <vinicius.limaferreira@hotmail.com>
2025-04-19 11:39:55 -0400

    Add get stock_prices function, store_prices function to store in minio and add minio installation on requirements.txt

--- udemy_airflow/dags/stock_market.py
+++ udemy_airflow/dags/stock_market.py
@@ -1,7 +1,12 @@
 from airflow.decorators import dag, task
 from airflow.hooks.base import BaseHook
 from airflow.sensors.base import PokeReturnValue
+from airflow.operators.python import PythonOperator
 from datetime import datetime
+from include.stock_market.tasks import _get_stock_prices
+
+SYMBOL = 'NVDA'
+
 
 @dag(
     start_date=datetime(2025,4,1),
@@ -12,7 +17,6 @@ from datetime import datetime
 def stock_market():
 
     @task.sensor(poke_interval=30, timeout=300, mode='poke')
-    
     def is_api_avaliable() -> PokeReturnValue:
         import requests
 
@@ -23,7 +27,19 @@ def stock_market():
         condition = response.json()['finance']['result'] is None
         return PokeReturnValue(is_done=condition , xcom_value=url)
 
-    is_api_avaliable()
+    get_stock_prices = PythonOperator(
+        task_id='get_stock_prices',
+        python_callable=_get_stock_prices,
+        op_kwargs={'url': '{{ti.xcom_pull(task_ids="is_api_avaliable")}}', 'symbol':SYMBOL}        
+    )
+
+    store_prices = PythonOperator(
+        task_id='store_prices',
+        python_callable=_store_prices,
+        
+    )
+
+    is_api_avaliable() >> get_stock_prices
 
 stock_market()
 #TESTING A DAG
---
+++ udemy_airflow/include/stock_market/tasks.py
@@ -0,0 +1,25 @@
+from airflow.hooks.base import BaseHook
+from minio import minio
+
+def _get_stock_prices(url, symbol):
+    import requests
+    import json
+
+    url = f"{url}{symbol}?metrics=high?&interval=1d&range=1y"
+    api = BaseHook.get_connection('stock_api')
+    response = requests.get(url, headers=api.extra_dejson['headers'])
+    return json.dumps(response.json()['chart']['result'][0])
+
+def _store_prices():
+    minio = BaseHook.get_connection('minio')
+    client = Minio(
+        endpoint=minio.extra_dejson['endpoint_url'].split('//')[1],
+        access_key=minio.login,
+        secret_key=minio.password,
+        secure=False
+    )
+    bucket_name = 'stock-market'
+    if not client.bucket_exists(bucket_name):
+        client.make_bucket(bucket_name)
+        
+
